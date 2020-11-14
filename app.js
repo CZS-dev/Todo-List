39d2523bc85c766c64b5a98d58272abe643ee800
Cazacu Sergiu <scazacu800@gmail.com>
2020-11-14 18:35:29 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// SELECTORS 
+
+const Input = document.getElementById('input');
+const Button = document.getElementById('button');
+const List = document.getElementById('list');
+
+// EVENT LISTENER
+
+Button.addEventListener('click', createTasks);
+
+// CREATE TASKS
+
+function createTasks (e) {
+    e.preventDefault();
+if (Input.value === '') {
+    alert("Write a task!");
+}else{
+    const newTask = document.createElement('li');
+    newTask.innerText = Input.value;
+    newTask.className = 'li';
+    List.appendChild(newTask);
+    Input.value = '';
+
+// TRASH BUTTON
+
+    const trashdButton = document.createElement('button');
+    trashdButton.innerHTML = '<i class="fas fa-trash"></i>';
+    newTask.appendChild(trashdButton);
+
+// CHECKED TASKS
+
+    newTask.addEventListener('click', (e) => newTask.classList.toggle('checked'));
+    
+
+// REMOVE TASKS    
+    
+    trashdButton.addEventListener('click', (e) => List.removeChild(newTask));
+       
+}
+}
